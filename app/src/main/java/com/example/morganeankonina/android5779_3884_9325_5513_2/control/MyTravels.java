67167b1e35f3139bf2f336f96fffafe2d87995af
ankonina <44062761+ankonina@users.noreply.github.com>
2019-01-23 18:19:09 +0200

    My travels option 2

---
+++ app/src/main/java/com/example/morganeankonina/android5779_3884_9325_5513_2/control/MyTravels.java
@@ -0,0 +1,15 @@
+package com.example.morganeankonina.android5779_3884_9325_5513_2.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.morganeankonina.android5779_3884_9325_5513_2.R;
+
+public class MyTravels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_travels);
+    }
+}
